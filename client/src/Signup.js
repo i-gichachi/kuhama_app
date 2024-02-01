f80dff8e0f785b2d2ec35986511a5ff66702a53e
i.gichachi <iangichachi@gmail.com>
2024-02-01 11:14:32 +0300

    Signup.js

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -22,7 +22,7 @@ const Signup = () => {
 
     const handleSubmit = async (values, { setSubmitting, setErrors }) => {
         try {
-            const response = await fetch('http://localhost:5555/signup', {
+            const response = await fetch('http://127.0.0.1:5555/signup', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
